e05153860d7a690287a7d6dd6f669a8ab395e721
shou-watanabe <watanabeshou1520@gmail.com>
2021-09-13 22:00:12 +0900

    [nestjs-practice]~ 46:00 ミドルウェアの設定

--- nest-practice/src/users/users.controller.ts
+++ nest-practice/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -12,7 +12,7 @@ export class UsersController {
   }
 
   @Post()
-  create(@Body() createUsers: CreateUserDto) {
+  create(@Body(ValidationPipe) createUsers: CreateUserDto) {
     return this.usersService.create(createUsers);
   }
 }
